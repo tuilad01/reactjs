7f783193f173676279c2de2c58631b80e203cc44
tuilad01 <thelegendaglx@gmail.com>
2019-10-11 15:17:26 +0700

    Add files via upload

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import './footer.css';
 
 class Footer extends Component {
@@ -12,9 +11,5 @@ class Footer extends Component {
         );
     }
 }
-
-Footer.propTypes = {
-
-};
 
 export default Footer;
