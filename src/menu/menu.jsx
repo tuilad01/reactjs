dabd1a62ea0dc2342b5d6f1b9986ef7984296b6e
dat truong <thelegendaglx@gmail.com>
2020-05-26 16:28:28 +0700

    log archive

--- src/menu/menu.jsx
+++ src/menu/menu.jsx
@@ -8,8 +8,13 @@ import { withRouter } from 'react-router-dom';
 
 import localStorageUtility from "../localStorageUtility";
 import config from "../config";
+import SpinnerLoading from "../spinner-loading/spinner-loading";
 
 class Menu extends Component {
+
+    state = {
+        loading: false
+    }
 
     clearCache(event) {
         event.preventDefault();
@@ -29,22 +34,28 @@ class Menu extends Component {
                     onClick: () => {
                         const strLearn = localStorageUtility.get(config.localStorage.learn)
 
+                        this.setState({ loading: true })                       
                         // push server
                         fetch(`${config.apiUrl}/archive`, {
                             method: "PUT",
                             headers: {
                                 'Content-Type': 'application/json'
                             },
-                            body: JSON.stringify({text: strLearn})
+                            body: JSON.stringify({ text: strLearn })
                         })
                             .then(res => res.json())
                             .then(result => {
                                 if (result && result._id) {
                                     window.location.reload()
                                 }
+
+                                this.setState({ loading: false })
                             })
                             .catch(error => {
+                                alert(error)
                                 console.error(error);
+
+                                this.setState({ loading: false })
                             })
                     }
                 },
@@ -67,16 +78,22 @@ class Menu extends Component {
                     label: 'Yes',
                     onClick: () => {
                         // get from server 
+                        this.setState({ loading: true })
                         fetch(`${config.apiUrl}/archive`)
                             .then(res => res.json())
-                            .then(result => {
+                            .then(result => {                                
                                 if (result && result._id) {
                                     localStorageUtility.set(config.localStorage.learn, JSON.parse(result.text))
                                     window.location.reload()
                                 }
+
+                                this.setState({ loading: false })
                             })
                             .catch(error => {
+                                alert(error)
                                 console.error(error);
+
+                                this.setState({ loading: false })
                             })
                     }
                 },
@@ -90,25 +107,36 @@ class Menu extends Component {
 
 
     render() {
-        return (
-            <section className="full-menu">
-                <main className="container">
-                    <div className="split-horizon">
-                        <a href="#menupanel" className="title-menu">Menu panel</a>
+        const { loading } = this.state
+        if (loading) {
+            return (
+                <section className="spinner-loading">
+                    <div className="flex-center">
+                        <SpinnerLoading />
                     </div>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/grammar">Grammar</a></li>
-                        <li><a href="https://tuilad01.github.io/#/word">Word</a></li>
-                        <li><a href="https://tuilad01.github.io/#/group">Group</a></li>
-                        <li><a href="#clearcache" onClick={e => this.clearCache(e)}>Clear cache</a></li>
-                        <li><a href="#archiveit" onClick={e => this.archiveIt(e)}>Archive it</a></li>
-                        <li><a href="#syncdata" onClick={e => this.syncIt(e)}>Sync it</a></li>
+                </section>
+            )
+        } else {
+            return (
+                <section className="full-menu">
+                    <main className="container">
+                        <div className="split-horizon">
+                            <a href="#menupanel" className="title-menu">Menu panel</a>
+                        </div>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/grammar">Grammar</a></li>
+                            <li><a href="https://tuilad01.github.io/#/word">Word</a></li>
+                            <li><a href="https://tuilad01.github.io/#/group">Group</a></li>
+                            <li><a href="#clearcache" onClick={e => this.clearCache(e)}>Clear cache</a></li>
+                            <li><a href="#archiveit" onClick={e => this.archiveIt(e)}>Archive it</a></li>
+                            <li><a href="#syncdata" onClick={e => this.syncIt(e)}>Sync it</a></li>
 
-                    </ul>
-                </main>
-            </section>
-        );
+                        </ul>
+                    </main>
+                </section>
+            );
+        }
     }
 }
 
