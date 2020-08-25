b51079aaa0bdcf8275e5f144c58eb4f9368fe8c3
dat truong <thelegendaglx@gmail.com>
2020-08-25 21:10:34 +0700

    refactor

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import localStorageUtil from '../localStorageUtility';
-import config from '../config';
+import dataAccess from '../dataAccess';
 
 // import component 
 import Nav from '../nav/nav';
@@ -33,7 +32,7 @@ class App extends Component {
       scrollTop: 0
     };
 
-    this.learnLocal = localStorageUtil.get(config.localStorage.learn)
+    this.learnLocal = dataAccess.getLearnLocal()
   }
 
   componentDidMount() {
@@ -46,212 +45,56 @@ class App extends Component {
     window.removeEventListener('scroll', this.handleScroll.bind(this));
   }
 
-  fetchNewGroup(arrGroup) {
-    if (!arrGroup || arrGroup.length === 0) return false;
+  render() {
+    const { groupFetch, wordFetch } = this.state;
 
-    const strLearnLocal = this.learnLocal;
-
-    if (!strLearnLocal) return false;
-    const learn = JSON.parse(strLearnLocal);
-    let arrKeyNotFound = [];
-    for (const key in learn) {
-      if (learn.hasOwnProperty(key)) {
-        const group = learn[key];
-
-        const groupNew = arrGroup.find(_ => _._id === group._id);
-
-        if (!groupNew) {
-          arrKeyNotFound.push(group._id);
-          continue;
-        }
-        // If performance is slowest. Should unblock blow comment. Just update group when add new or delete a word.
-        //if (groupNew.words.length === group.words.length) continue;
-
-        // Word not found in group local
-        group.words.filter(_ => !groupNew.words.find(__ => __._id === _._id)).map(word => {
-          group.words = group.words.filter(_ => _._id !== word._id);
-          group.state1 = group.state1.filter(_ => _._id !== word._id);
-          group.state2 = group.state2.filter(_ => _._id !== word._id);
-          group.state3 = group.state3.filter(_ => _._id !== word._id);
-          return word;
-        });
-
-        // Update Word change mean and name
-        group.words.map(word => {
-          const wordNew = groupNew.words.find(_ => _._id === word._id);
-          if (!wordNew) return word;
-
-          word.name = wordNew.name;
-          word.mean = wordNew.mean;
-
-          const wordIndexInState1 = group.state1.findIndex(_ => _._id === word._id);
-          if (wordIndexInState1 >= 0) {
-            group.state1[wordIndexInState1].name = wordNew.name;
-            group.state1[wordIndexInState1].mean = wordNew.mean;
-          } else {
-            const wordIndexInState2 = group.state2.findIndex(_ => _._id === word._id);
-            if (wordIndexInState2 >= 0) {
-              group.state2[wordIndexInState2].name = wordNew.name;
-              group.state2[wordIndexInState2].mean = wordNew.mean;
-            } else {
-              const wordIndexInState3 = group.state3.findIndex(_ => _._id === word._id);
-              if (wordIndexInState3 >= 0) {
-                group.state3[wordIndexInState3].name = wordNew.name;
-                group.state3[wordIndexInState3].mean = wordNew.mean; 
-              } 
-            }
-          }
-
-          return word;
-        });
-
-        // New word in group
-        groupNew.words.filter(_ => !group.words.find(__ => __._id === _._id)).map(word => {
-          group.words.push({
-            _id: word._id,
-            name: word.name,
-            mean: word.mean,
-          })
-          group.state1.push({
-            _id: word._id,
-            name: word.name,
-            mean: word.mean,
-            flipped: false,
-            display: true
-          });
-          return word;
-        });
-        
-
-        group.words = groupNew.words;
-        group.percent = Math.round((group.state3.length * 100) / group.words.length);
-      }
+    if (groupFetch.error || wordFetch.error) {
+      return (
+        <section className="flex-center color-red">
+          <p>Error fetch group: {groupFetch.error}</p>
+          <p>Error fetch word: {wordFetch.error}</p>
+        </section>
+      );
     }
-
-    if (arrKeyNotFound.length > 0) {
-      arrKeyNotFound.map(key => {
-        delete learn[key];
-        return key;
-      });
-    }
-
-
-    localStorageUtil.set(config.localStorage.learn, learn);
-    this.learnLocal = JSON.stringify(learn)
-    return true
-  }
-
-  _createGroup = () => {
-    return {
-      _id: '',
-      name: '',
-      description: '',
-      words: [],
-      state: 1,
-      state1: [],
-      state2: [],
-      state3: [],
-      percent: 1,
-      learnNumberTimes: 0
-    }
-  }
-
-  createSimilarGroup = () => {
-    const strLearnLocal = this.learnLocal
-    let learn = {}
-    if (strLearnLocal) {
-      learn = JSON.parse(strLearnLocal);
-      if (learn[config.localStorage.similarGroup]) {
-        return false;
-      }
-    }
-
-    learn[config.localStorage.similarGroup] = this._createGroup()
-    learn[config.localStorage.similarGroup]._id = config.localStorage.similarGroup
-    learn[config.localStorage.similarGroup].name = 'Similar words'
-    learn[config.localStorage.similarGroup].description = 'Similar words'
-
-    localStorageUtil.set(config.localStorage.learn, learn);
-    this.learnLocal = JSON.stringify(learn)
-  }
-
-  createForgetGroup = () => {
-    const strLearnLocal = this.learnLocal
-    let learn = {}
-    if (strLearnLocal) {
-      learn = JSON.parse(strLearnLocal);
-      if (learn[config.localStorage.forgetGroup]) {
-        return false;
-      }
-    }
-
-    learn[config.localStorage.forgetGroup] = this._createGroup()
-    learn[config.localStorage.forgetGroup]._id = config.localStorage.forgetGroup
-    learn[config.localStorage.forgetGroup].name = 'Forget words'
-    learn[config.localStorage.forgetGroup].description = 'Forget words'
-
-    localStorageUtil.set(config.localStorage.learn, learn);
-    this.learnLocal = JSON.stringify(learn)
-  }
-
-  initLocalData = () => {
-    const localGroups = localStorageUtil.getArray(config.localStorage.groups);
-
-    if (localGroups.length <= 0) {
-      fetch(`${config.apiUrl}/group/all`)
-        .then(res => res.json())
-        .then(result => {
-          localStorageUtil.set(config.localStorage.groups, result);
-          
-          this.fetchNewGroup(result)
-
-          this.setState((state) => {
-            state.groupFetch.isLoaded = true;
-            return state;
-          });
-
-          return result
-        })
-        .catch(error => {
-          this.setState((state) => {
-            state.groupFetch.isLoaded = true;
-            state.groupFetch.error = error;
-            return state;
-          });
-          return error
-        });
+    else if (!groupFetch.isLoaded || !wordFetch.isLoaded) {
+      return (
+        <section>
+          <div className="flex-center">
+            <SpinnerLoading />
+          </div>
+          <h1 className="color-white text-center">Initializing System, Please Wait For Minutes!</h1>
+        </section>
+      );
     } else {
-      this.setState((state) => {
-        state.groupFetch.isLoaded = true;
-      });
-    }
+      return (
+        <Router>
+          <div>
+            <Nav onClickShowMenu={this.callOpenMenu.bind(this)} />
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/learn/:id" component={Learn} />
+              <Route exact path="/grammar" component={GrammarTense} />
+              <Route component={Notfound} />
+            </Switch>
+            {this.state.isShowMenu ? <NavMenu /> : ""}
 
-    const localWords = localStorageUtil.get(config.localStorage.words);
-    if (!localWords) {
-      fetch(`${config.apiUrl}/word/total`)
-        .then(res => res.json())
-        .then(result => {
-          localStorageUtil.set(config.localStorage.words, result);
-          this.setState((state) => {
-            state.wordFetch.isLoaded = true;
-            return state;
-          });
-          return result
-        })
-        .catch(error => {
-          this.setState((state) => {
-            state.wordFetch.isLoaded = true;
-            state.wordFetch.error = error;
-            return state;
-          });
-          return error
-        });
-    } else {
-      this.setState((state) => {
-        state.wordFetch.isLoaded = true;
-        return state;
-      });
+            <Footer />
+
+            {this.scrollTop()}
+          </div>
+        </Router>
+      );
     }
+  }
+
+  initLocalData() {
+    dataAccess.getGroup()
+    this.setState(state => { state.groupFetch.isLoaded = true; return state });
+    dataAccess.getTotalNumberWord()
+    this.setState(state => { state.wordFetch.isLoaded = true; return state });
+
+    dataAccess.createSimilarGroup()
+    dataAccess.createForgetGroup()
   }
 
   callOpenMenu() {
@@ -281,53 +124,6 @@ class App extends Component {
               </i>
       </div>
     );
-  }
-
-
-  render() {
-    const { groupFetch, wordFetch } = this.state;
-
-    if (groupFetch.error || wordFetch.error) {
-      return (
-        <section className="flex-center color-red">
-          <p>Error fetch group: {groupFetch.error}</p>
-          <p>Error fetch word: {wordFetch.error}</p>
-        </section>
-      );
-    }
-    else if (!groupFetch.isLoaded || !wordFetch.isLoaded) {
-      return (
-        <section>
-          <div className="flex-center">
-            <SpinnerLoading />
-          </div>
-          <h1 className="color-white text-center">Initializing System, Please Wait For Minutes!</h1>
-        </section>
-      );
-    } else {
-     
-      this.createSimilarGroup()
-      this.createForgetGroup()
-
-      return (
-        <Router>
-          <div>
-            <Nav onClickShowMenu={this.callOpenMenu.bind(this)} />
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route exact path="/learn/:id" component={Learn} />
-              <Route exact path="/grammar" component={GrammarTense} />
-              <Route component={Notfound} />
-            </Switch>
-            {this.state.isShowMenu ? <NavMenu /> : ""}
-
-            <Footer />
-
-            {this.scrollTop()}
-          </div>
-        </Router>
-      );
-    }
   }
 }
 
--- src/dataAccess.js
+++ src/dataAccess.js
@@ -48,4 +48,253 @@ dataAccess.getWordHasGroup = function () {
   return words.flat();
 }
 
+dataAccess.setLearnLocal = function (learnLocal) {
+  if (!learnLocal) throw new Error("function [setLearnLocal] Error: invalid parameters")
+  try {
+    localStorageUtility.set(config.localStorage.learn, learnLocal);
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+dataAccess.setGroupLocal = function (groups) {
+  if (!groups) throw new Error("function [setGroupLocal] Error: invalid parameters")
+  try {
+    localStorageUtility.set(config.localStorage.groups, groups);
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+dataAccess.setWordLocal = function (totalNumber) {
+  if (totalNumber < 0) throw new Error("function [setWordLocal] Error: invalid parameters")
+  try {
+    localStorageUtility.set(config.localStorage.words, totalNumber);
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+dataAccess.getGroupServer = function () {
+  try {
+    return fetch(`${config.apiUrl}/group/all`)
+      .then(res => res.json())
+      .then(groups => {
+        return _handleData(groups)
+      })
+      .catch(error => {
+        return _handleError(error)
+      });
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+dataAccess.getTotalNumberWordServer = function () {
+  try {
+    return fetch(`${config.apiUrl}/word/total`)
+      .then(res => res.json())
+      .then(result => {
+        return _handleData(result)
+      })
+      .catch(error => {
+        return _handleError(error)
+      });
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+dataAccess.getGroup = async function () {
+  try {
+    const localGroups = dataAccess.getGroupLocal();
+
+    if (localGroups.length <= 0) {
+      const result = await dataAccess.getGroupServer()
+
+      if (result.isOk) {
+        const groups = result.data
+        dataAccess.setGroupLocal(groups)
+
+        // TODO: update learn local
+        dataAccess.updateLearnLocal(groups)
+
+        // Return groups
+        return groups
+      } else {
+        console.error(result.errorMessage)
+      }
+    } else {
+      return localGroups
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
+
+  return []
+}
+
+
+dataAccess.getTotalNumberWord = async function () {
+  try {
+    const localWords = dataAccess.getWordLocal()
+
+    if (!localWords) {
+      const result = await dataAccess.getTotalNumberWordServer()
+
+      if (result.isOk) {
+        const totalNumberWord = result.data
+        dataAccess.setWordLocal(totalNumberWord)
+        // Return total number of words
+        return totalNumberWord
+      } else {
+        console.error(result.errorMessage)
+      }
+    } else {
+      return localWords
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
+
+  return 0
+}
+
+dataAccess.updateLearnLocal = function (groups) {
+  if (!groups) throw new Error("function [updateLearnLocal] Error: invaild parameters")
+
+  try {
+    const learnLocal = dataAccess.getLearnLocal()
+    if (!learnLocal) return false
+
+    const learnLocalGroupKeys = Object.keys(learnLocal)
+    // Remove group in learn local
+    for (let i = 0; i < groups.length; i++) {
+      const { _id, name, description, words } = groups[i];
+
+      // Remove group nonexist
+      if (learnLocalGroupKeys.indexOf(_id) === -1) {
+        if (learnLocal[_id]) {
+          delete learnLocal[_id]          
+        }
+        continue
+      }
+
+      let { state1, state2, state3 } = learnLocal[_id]
+      const wordIds = learnLocal[_id].words.map(_ => _._id)
+
+      // Remove and update words group of learn local
+      state1 = _removeAndUpdateWordInState(state1)
+      state2 = _removeAndUpdateWordInState(state2)
+      state3 = _removeAndUpdateWordInState(state3)
+
+      // Add new words into state1 group of learn local
+      const newWords = words
+        .filter(_ => wordIds.indexOf(_._id) === -1)
+        .map(word => dataAccess.createModelWordLearn(word))
+      state1 = [...state1, newWords]
+
+      // Update all into group of learn local
+      learnLocal[_id] = { ...learnLocal[_id], name, description, words, state1, state2, state3 }
+    }
+
+    // Store learn local
+    dataAccess.setLearnLocal(learnLocal)
+  } catch (error) {
+    console.error(error.message)
+    return false
+  }
+
+  return true
+}
+
+dataAccess.createModelWordLearn = (word) => {
+  if (!word) throw new Error("function [createModelWordLearn] Error: invaild parameters")
+
+  return {
+    _id: word._id,
+    name: word.name,
+    mean: word.mean,
+    flipped: false,
+    display: true
+  }
+}
+
+dataAccess.createModelGroupLearn = () => {
+  return {
+    _id: '',
+    name: '',
+    description: '',
+    words: [],
+    state: 1,
+    state1: [],
+    state2: [],
+    state3: [],
+    percent: 1,
+    learnNumberTimes: 0
+  }
+}
+
+dataAccess.createSimilarGroup = function () {  
+  let learnLocal = dataAccess.getLearnLocal()
+  if (!learnLocal || learnLocal[config.localStorage.similarGroup]) {
+    return false
+  }
+
+  const similarGroup = dataAccess.createModelGroupLearn()
+  similarGroup._id = config.localStorage.similarGroup
+  similarGroup.name = 'Similar words'
+  similarGroup.description = 'Similar words'
+
+  learnLocal[config.localStorage.similarGroup] = similarGroup
+  dataAccess.setLearnLocal(learnLocal)
+}
+
+dataAccess.createForgetGroup = function () {
+  let learnLocal = dataAccess.getLearnLocal()
+  if (!learnLocal || learnLocal[config.localStorage.forgetGroup]) {
+    return false
+  }
+
+  const forgetGroup = dataAccess.createModelGroupLearn()
+  forgetGroup._id = config.localStorage.forgetGroup
+  forgetGroup.name = 'Forget words'
+  forgetGroup.description = 'Forget words'
+
+  learnLocal[config.localStorage.similarGroup] = forgetGroup
+  dataAccess.setLearnLocal(learnLocal)
+}
+
+// Private functions
+const _handleData = (data) => {
+  return {
+    isOk: true,
+    data
+  }
+}
+
+const _handleError = (error) => {
+  return {
+    isOk: false,
+    errorMessage: error.message
+  }
+}
+
+const _removeAndUpdateWordInState = (state, words, wordIds) => {
+  return state.filter(word => {
+    const index = wordIds.indexOf(word._id)
+    // Update both name and mean word of group in learn local
+    if (index >= 0) {
+      const { name, mean } = words[index]
+      word.name = name
+      word.mean = mean
+      return true
+    }
+    return false
+  })
+}
+
+
+
+
 export default dataAccess;
